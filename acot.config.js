967a7a0ab716aab91e5f4cea6b529ccd37eec3eb
odan <odan3240@gmail.com>
2021-01-11 06:46:29 +0900

    fix rule

--- acot.config.js
+++ acot.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  extends: ['@acot'],
+  // extends: ['@acot'],
+  presets: ['@acot/wcag'],
+  rules: { "@acot/wcag/interactive-supports-focus": "error" },
   origin: 'https://interactive-supports-focus-sandbox.netlify.app',
   paths: ['/'],
 };
